30ac73f5e35e8b9693f4804114c36da1b1fa1b1d
Alex Delpisheh <alex.delpisheh@gmail.com>
2025-12-14 21:59:54 -0500

    refactor: Add Factory Pattern to the class

--- src/JobTracker.Domain/Entities/JobApplication.cs
+++ src/JobTracker.Domain/Entities/JobApplication.cs
@@ -14,6 +14,45 @@ public class JobApplication
     public DateTime? NextFollowUpAt { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime? LastModifiedAt { get; private set; }
+
+    private JobApplication()
+    {
+    }
+
+    public static JobApplication Create(string companyName, string jobName, string jobDescription,
+        string? jobUrl = null, string? notes = null, int followUpInDays = 7)
+    {
+        if (string.IsNullOrWhiteSpace(companyName))
+            throw new ArgumentException($"'{nameof(companyName)}' cannot be null or empty and it is required.",
+                nameof(companyName));
+        if (string.IsNullOrWhiteSpace(jobName))
+            throw new ArgumentException($"'{nameof(jobName)}' cannot be null or empty and it is required.",
+                nameof(jobName));
+        if (string.IsNullOrWhiteSpace(jobName))
+            throw new ArgumentException($"'{nameof(jobName)}' cannot be null or empty and it is required.",
+                nameof(jobName));
+        if (string.IsNullOrWhiteSpace(jobDescription))
+            throw new ArgumentException($"'{nameof(jobDescription)}' cannot be null or empty and it is required.",
+                nameof(jobDescription));
+        if (followUpInDays < 0)
+            throw new ArgumentOutOfRangeException($"'{nameof(followUpInDays)} must not be less than 0'",
+                nameof(followUpInDays));
+
+
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            CompanyName = companyName.Trim(),
+            JobName = jobName.Trim(),
+            JobDescription = jobDescription.Trim(),
+            JobUrl = jobUrl?.Trim(),
+            Notes = notes?.Trim(),
+            Status = ApplicationStatus.Applied,
+            AppliedAt = DateTime.UtcNow,
+            NextFollowUpAt = DateTime.UtcNow.AddDays(followUpInDays),
+            CreatedAt = DateTime.UtcNow,
+        };
+    }
 }
 
 public enum ApplicationStatus
@@ -25,5 +64,5 @@ public enum ApplicationStatus
     Accepted = 4,
     Declined = 5,
     Rejected = 6,
-    NoResponse = 7 
+    NoResponse = 7
 }
